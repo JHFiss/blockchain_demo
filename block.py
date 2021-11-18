8733867fab2b4f07e8175f9ec1a8b5b74718ed23
Jan H. Fiss <jhfiss@gmail.com>
2021-11-18 23:48:07 +0100

    Added simple __str__function for a Block First test of chaining

--- block.py
+++ block.py
@@ -15,7 +15,7 @@ class Block(Generic[T]):
     time_stamp: int
     nonce = 0
 
-    def calc_this_hash(self):
+    def calc_hash(self):
         """
         Generates a hash for a block instance
         """
@@ -28,25 +28,37 @@ class Block(Generic[T]):
         Generates new hashes and increases nonce until the created hash contains the value defined in ``mine_cond``
         """
         mine_cond: string = '300597'
-        mine_hash: string = self.calc_this_hash()
+        mine_hash: string = self.calc_hash()
         while mine_cond not in mine_hash:
             self.nonce += 1
-            mine_hash = self.calc_this_hash()
+            mine_hash = self.calc_hash()
             # Print the current value of nonce for debug and demonstration purposes
             print(self.nonce)
+        self.this_hash = mine_hash
         return mine_hash
 
     def __init__(self, prev_hash: string, data: T, timestamp: int):
         self.prev_hash = prev_hash
         self.data = data
         self.time_stamp = timestamp
-        self.this_hash = self.calc_this_hash()
+        self.this_hash = self.calc_hash()
 
         # print the values of a block after initialisation for debug and demonstration purposes
         print(self.this_hash)
         print(self.prev_hash)
         print(self.data)
         print(self.time_stamp)
+
+    def __str__(self):
+        ret = ''
+        ret += '{\n'
+        ret += 'previous Hash: ' + self.prev_hash + '\n'
+        ret += 'Hash: ' + self.this_hash + '\n'
+        ret += 'Data: ' + str(self.data) + '\n'
+        ret += 'Nonce: ' + str(self.nonce) + '\n'
+        ret += 'Timestamp: ' + str(self.time_stamp) + '\n'
+        ret += '}'
+        return ret
 
 
 
--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-import blockchain
+import block
 import time
-bl = blockchain.Block('apvs', 'testdata', time.time())
+bl = block.Block('apvs', 'testdata', time.time())
 print(bl.mine_this_block())
+l = [bl]
+for x in range(1, 5):
+    prev_hash = l.__getitem__(x-1).this_hash
+    data = 'testdata' + str(x)
+    next_block = block.Block(prev_hash, data, time.time())
+    next_block.mine_this_block()
+    l.append(next_block)
+
+for x in range(0, 5):
+    print(str(l.__getitem__(x)))
+
+
